34a46c4c94aca19452eaf956beebfe9a91ffbdde
The Dumb <info@hemisphera.com>
2026-02-26 18:01:56 +0100

    Update installation script

--- Install.ps1
+++ Install.ps1
@@ -1,12 +1,10 @@
-if (-not (([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)))
-{
+if (-not (([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))) {
     $arguments = "& '" + $myinvocation.mycommand.definition + "'"
     Start-Process powershell -Verb runAs -ArgumentList $arguments
     Break
 }
 
-try
-{
+try {
     $ErrorActionPreference = "Stop"
 
     $ReaperResBaseFolder = "$env:APPDATA\REAPER"
@@ -19,8 +17,6 @@ try
         $ReaperFolder = "$ReaperResBaseFolder\$($Subfolder.Name)\Hemisphera"
         if (Test-Path $ReaperFolder) {
             Write-Host "Deleting existing folder '$ReaperFolder'"
-
-            # Resorting to System.IO because Remove-Item apparently is bugged
             [IO.Directory]::Delete($ReaperFolder)
         }
         Write-Host "Creating link from '$ReaperFolder' to '$($Subfolder.FullName)'"
@@ -29,8 +25,7 @@ try
 
     Write-Host -ForegroundColor Green "Successfully completed."
 }
-catch
-{
+catch {
     Write-Host -ForegroundColor Red $_
 }
 Write-Host "Press ENTER to exit"
